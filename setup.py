f4ef332a4b96e4af1bfe5dfee276cc1801d54ba5
Joseph Chazalon <jchazalon@users.noreply.github.com>
2021-11-23 19:00:39 +0100

    Update setup.py
    - Install tensorflow 2 package
    - Use OpenCV Headless

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     ],
     install_requires=[
         'numpy',
-        'opencv-python',
+        'opencv-python-headless',
         'lxml',
         'scipy',
         'numba',
@@ -29,7 +29,7 @@ setup(
         'brnolm>=0.1.1',
         'scikit-learn',
         'scikit-image',
-        'tensorflow-gpu==1.15',
+        'tensorflow>=2',
         'shapely',
         'pyamg',
         'imgaug',
